79e262e719c987a3b84d0e8cba4d59348e3e07e4
emersonpisoni <emersonpisoni14@gmail.com>
2026-03-15 10:36:23 -0300

    add new route

--- routing-react/src/main.jsx
+++ routing-react/src/main.jsx
@@ -22,6 +22,10 @@ const router = createBrowserRouter([
         path: "about",
         element: <About />,
       },
+      {
+        path: "contact",
+        element: <Contact />,
+      }
     ],
   },
 ]);
