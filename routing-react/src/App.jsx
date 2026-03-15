1749f8b95e6e31ceaf5b4cd5d4f1f698e5fdfdc0
emersonpisoni <emersonpisoni14@gmail.com>
2026-03-15 10:36:23 -0300

    create new routes

--- routing-react/src/App.jsx
+++ routing-react/src/App.jsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
     <div>
       <h1>React Routing Study</h1>
-      <p>Project setup complete.</p>
+      <Outlet />
     </div>
   );
 }
--- routing-react/src/main.jsx
+++ routing-react/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Home from "./pages/home";
+import About from "./pages/about";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
